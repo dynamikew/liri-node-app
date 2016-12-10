84ba9e7624c839a080e6dfd6f725d4374150c6e9
michael james waldron <dynamikew@michaels-MacBook-Pro.local>
2016-12-10 12:47:08 -0500

    added code from npm

--- liri.js
+++ liri.js
@@ -1,3 +1,46 @@
 var twitter = require("twitter");
 var spotify = require("spotify");
-var twitter = require("twitter");
+var imdb = require('imdb');
+
+// imdb pulls
+
+// ----------------------------------------------------------------
+
+imdb('tt3659388', function(err, data) {
+  if(err)
+    console.log(err.stack);
+ 
+  if(data)
+    console.log(data);
+});
+
+// Spotify pulls
+
+// ----------------------------------------------------------------
+ 
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    // Do something with 'data' 
+});
+ 
+// Twitter pulls
+
+// ----------------------------------------------------------------
+
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
